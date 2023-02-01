ac8e3ec643faee56c32da054ff6d35953c43c355
arshad ansari <arshadchennaisbg.apj.369@gmail.com>
2023-02-01 23:38:30 +0530

    Get Request

--- src/main/java/com/arshadansari/Springboot/demo/service/DepartmentService.java
+++ src/main/java/com/arshadansari/Springboot/demo/service/DepartmentService.java
@@ -2,7 +2,10 @@ package com.arshadansari.Springboot.demo.service;
 
 import com.arshadansari.Springboot.demo.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     Department saveDepartment(Department department);
 
+    List<Department> fetchDepartmentList();
 }
--- src/main/java/com/arshadansari/Springboot/demo/service/DepartmentServiceIpl.java
+++ src/main/java/com/arshadansari/Springboot/demo/service/DepartmentServiceIpl.java
@@ -5,6 +5,8 @@ import com.arshadansari.Springboot.demo.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DepartmentServiceIpl implements DepartmentService{
 
@@ -14,5 +16,10 @@ public class DepartmentServiceIpl implements DepartmentService{
     @Override
     public Department saveDepartment(Department department) {
         return departmentRepository.save(department);
+    }
+
+    @Override
+    public List<Department> fetchDepartmentList() {
+        return departmentRepository.findAll();
     }
 }
